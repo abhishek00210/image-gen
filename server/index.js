05ee8dfc17111f00f329e7a2ba23caf37a5c8026
Abhishek Kumar Rai <75179538+abhishek00210@users.noreply.github.com>
2024-09-21 01:08:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://image-gen-frontend-topaz.vercel.app', // Allow your frontend's URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
